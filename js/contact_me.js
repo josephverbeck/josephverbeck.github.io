573b4d5e442130a38d758b4436001d5f31bc8a80
josephverbeck <joseph.verbeck@nih.gov>
2016-12-08 17:25:59 -0500

    Adding in contact me post

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://node-josephverbeck-api.herokuapp.com/create_user",
                 type: "POST",
                 data: {
                     name: name,
